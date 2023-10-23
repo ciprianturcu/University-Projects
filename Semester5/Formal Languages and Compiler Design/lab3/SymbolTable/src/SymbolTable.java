4e95997a59ce9324c73dd99ecd0fefd63689746c
ciprianturcu <ciprianstelian_08@yahoo.com>
2023-10-23 21:00:57 +0300

    finished

---
+++ Semester5/Formal Languages and Compiler Design/lab3/SymbolTable/src/SymbolTable.java
@@ -0,0 +1,22 @@
+public class SymbolTable<K> {
+    private HashTable<K> hashTable;
+
+    public SymbolTable(HashTable<K> hashTable) {
+        this.hashTable = hashTable;
+    }
+
+    public HashTable<K> getHashTable() {
+        return hashTable;
+    }
+
+    public int add(K symbol) {
+        return this.hashTable.insertNode(symbol);
+    }
+    public int getPosition(K symbol) {
+        return this.hashTable.getPosition(symbol);
+    }
+
+    public K getByPosition(int position){
+        return this.hashTable.getByPosition(position);
+    }
+}
