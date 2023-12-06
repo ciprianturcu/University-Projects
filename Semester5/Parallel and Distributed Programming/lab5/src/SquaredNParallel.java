c0661b0e6241d1556fe5f4f91fcd75bc5432f114
ciprianturcu <ciprianstelian_08@yahoo.com>
2023-12-06 14:25:23 +0200

    done

--- Semester5/Parallel and Distributed Programming/lab5/src/SquaredNParallel.java
+++ Semester5/Parallel and Distributed Programming/lab5/src/SquaredNParallel.java
@@ -1,34 +1,27 @@
-import java.lang.annotation.Target;
-import java.util.Arrays;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 public class SquaredNParallel {
 
     private static final int THREAD_NUMBER = 4;
-    private Integer[] firstPolynomial;
-    private Integer[] secondPolynomial;
-    private Integer[] resultPolynomial;
 
-    public SquaredNParallel(Integer[] firstPolynomial, Integer[] secondPolynomial) {
-        this.firstPolynomial = firstPolynomial;
-        this.secondPolynomial = secondPolynomial;
-        this.resultPolynomial = new Integer[firstPolynomial.length + secondPolynomial.length - 1];
-        Arrays.fill(resultPolynomial, 0);
-    }
-
-
-    public void multiplyPolynomials() {
+    public void multiplyPolynomials(Integer[] firstPolynomial, Integer[] secondPolynomial, Integer[] resultPolynomial) throws InterruptedException {
         ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(THREAD_NUMBER);
         int nrOfTasks = resultPolynomial.length / THREAD_NUMBER;
         if (nrOfTasks == 0) {
             nrOfTasks = 1;
         }
         int end;
-        for(int i=0;i<resultPolynomial.length;i++)
+        for(int i=0;i<resultPolynomial.length;i+=nrOfTasks)
         {
             end = i+nrOfTasks;
-            Task task = new Task()
+            Task task = new Task(i, end, firstPolynomial, secondPolynomial, resultPolynomial);
+            executor.execute(task);
         }
+        executor.shutdown();
+        executor.awaitTermination(100, TimeUnit.SECONDS);
     }
+
+
 }
--- Semester5/Parallel and Distributed Programming/lab5/src/SquaredNSequential.java
+++ Semester5/Parallel and Distributed Programming/lab5/src/SquaredNSequential.java
@@ -1,23 +1,13 @@
 import java.util.Arrays;
 
 public class SquaredNSequential {
-    private Integer[] firstPolynomial;
-    private Integer[] secondPolynomial;
 
-    public SquaredNSequential(Integer[] firstPolynomial, Integer[] secondPolynomial) {
-        this.firstPolynomial = firstPolynomial;
-        this.secondPolynomial = secondPolynomial;
-    }
-
-    public Integer[] multiplyPolynomials() {
-        Integer[] result = new Integer[firstPolynomial.length + secondPolynomial.length - 1];
-        Arrays.fill(result, 0);
+    public void multiplyPolynomials(Integer[] firstPolynomial, Integer[] secondPolynomial, Integer[] resultPolynomial) {
         for (int indexFirst = 0; indexFirst < firstPolynomial.length; indexFirst++) {
             for (int indexSecond = 0; indexSecond < secondPolynomial.length; indexSecond++) {
-                result[indexFirst + indexSecond] += firstPolynomial[indexFirst] * secondPolynomial[indexSecond];
+                resultPolynomial[indexFirst + indexSecond] += firstPolynomial[indexFirst] * secondPolynomial[indexSecond];
             }
         }
-        return result;
     }
 
     public static String printPoly(Integer[] polynomial) {
@@ -32,13 +22,5 @@ public class SquaredNSequential {
             }
         }
         return stringBuilder.toString();
-    }
-
-    @Override
-    public String toString() {
-        return "SquaredNSequential method : " +
-                "firstPolynomial = " + printPoly(firstPolynomial) +
-                ", secondPolynomial=" + printPoly(secondPolynomial) +
-                '}';
     }
 }
